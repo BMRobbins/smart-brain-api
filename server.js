2daff532801063ca8342a40feb139e2170618f3c
Brian <brobbins1984@gmail.com>
2019-02-16 19:48:40 -0800

    another database fix

--- server.js
+++ server.js
@@ -13,10 +13,9 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-silhouetted-74659',
-    user : 'brobbins',
-    password : '',
-    database : 'smart-brain'
+    host : process.env.DATABASE_URL,
+    ssl:true,
+
   }
 });
 
